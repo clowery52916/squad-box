31c2aff3800fa2663a7156eb8703c9aac5232469
clowery <clowery52916@gmail.com>
2018-04-09 18:40:54 -0400

    created seeds, getting a error stating user and post must exist in comments, but they were added to seeded data, working through the issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   has_many :comments
   has_many :posts
+  validates :password, presence: true, length: { minimum: 10}
+  validates :email, confirmation: true
+  validates :age, numericality: { message: 'You are over the allowed age, your age is %{value}, you are not permitted to enter this site.' }
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+court = User.create!(name: 'Courtney' , photo: 'https://i.imgur.com/I6wdfHL.jpg', password: 'qwerty1234', age: 34, email: 'clowery52916@gmail.com',  )
+
+my_post = Post.create!(post_body: 'Hello! I love this app!!!', like: 0, user_id: court.id)
+
+my_comment = Comment.create!(comment_body: 'I am so happy!', like: 2, user_id: court.id, post_id: my_post.id)
