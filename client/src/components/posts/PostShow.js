a529a1447ca4952b8cf4b9ed981685481670221d
clowery <clowery52916@gmail.com>
2018-04-19 18:21:30 -0400

    resetting up heroku

--- client/src/components/posts/PostShow.js
+++ client/src/components/posts/PostShow.js
@@ -6,51 +6,85 @@ import Navbar from '../navbar/Navbar.js'
 
 class PostShow extends Component {
 
-    componentWillMount() {
-        const postId = this.props.match.params.postId;
-        const userId = this.props.match.params.userId;
-        this
-            .props
-            .getOnePostRoute(userId, postId)
+  state = {
+      showEdit: false,
+      redirectToUsers: false
+  }
 
-    }
-    componentWillReceiveProps(nextProps) {
-        this.setState({
-            posts: {
-                body: this.props.match.params.postId,
-          
-            }
-        })
-    }
+  componentDidMount() {
+      this.getPost()
+  }
 
-    state = {
-        posts: {
-            body:''
+  getPost = async() => {
+      const res = await axios.get(`/api/users/${this.props.match.params.userId}`)
+      this.setState({
+          body: res.data.user.body,
 
-        }
-    }
+      })
 
-    render() {
-        console.log("our post", this.props.posts)
-        return (
+  }
 
-            <Container>
-                <div>
-                    <Navbar/>
-                </div>
-                <h1>Post</h1>
-                <br/>
-                <SinglePost>
-                    <h3>{this.state.posts.title}</h3>
-                    <br/>
-                    <p>{this.state.posts.content}</p>
-                </SinglePost>
+  handleSubmit = async(event) => {
+      event.preventDefault()
+      console.log(this.state.body)
+      const res = axios.patch(`/api/users/${this.props.match.params.userId}`,{
+          body: this.state.body,
+          aboutMe: this.state.aboutMe
+      })
+  }
 
-            </Container>
+  handleChange = (event) => {
+      const body = event.target.body
+      const newState = { ...this.state }
+      newState[body] = event.target.value
+      this.setState(newState)
+  }
 
-        );
-    }
+  toggle = () => {
+      this.setState({ showEdit: !this.state.showEdit })
+  }
+
+  handleDelete = async() => {
+      const res = axios.delete(`/api/users/${this.props.match.params.userId}`)
+      console.log(res)
+      this.setState({ redirectToUsers: !this.state.redirectToUsers })
+      // this.props.getPosts()
+
+  }
+
+  render() {
+      if(this.state.redirectToUsers){
+          return <Redirect to='/users/userId' render={UserIndex}/>
+      }
+      return (
+          <div className='search list' >
+              <h1>{this.state.body}</h1>
+              <button onClick={this.toggle}>Edit Your Post</button>
+              <button onClick={this.handleDelete}>Delete User Account</button>
+              {this.state.showEdit
+                  ?
+                  <div>
+                      <form onSubmit={this.handleSubmit}>
+
+                          <input body='body'
+                              type='text'
+                              onChange={this.handleChange}
+                              value={this.state.body}
+                              placeholder={this.state.body}
+                          />
+
+                          <button type='submit'>Update User</button>
+
+
+
+                      </form>
+                  </div>
+                  : null}
+          </div>
+      );
+  }
 }
+
 
 const mapStateToProps = (state) => {
     return {posts: state.posts[0]}
